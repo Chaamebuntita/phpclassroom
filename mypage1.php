cf13413e9dcb5d2f5c4669a2e2bd12691904af8f
lsw-56706 <98399961+Chaamebuntita@users.noreply.github.com>
2022-02-09 22:03:32 +0700

    Create mypage1.php

---
+++ mypage1.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>การเชื่อมโยงเว็บไซต์</title>
+  </head>
+  <body>
+    <a href="https://https://facebook.com"><h1>วาร์ปของฉัน</h1></a>
+    <a href="contact.php"><h1>ติดต่อฉัน</h1></a>
+     <a href="contact.php"><h1>ไปด้านล่าง</h1></a>
