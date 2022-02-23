73d22393455f7467972e40456d62cf69dfe46203
lsw-56706 <98399961+Chaamebuntita@users.noreply.github.com>
2022-02-23 13:31:54 +0700

    Update font.php

--- font.php
+++ font.php
@@ -6,12 +6,20 @@
         font-family: "MyFont1";
         src: url("Chocolateteehaypai.zip");
       }
-      body{
+      @font-face{
+        font-family: "MyFont2";
+        src: url("MiPancake.zip");
+      }
+      #font1{
         font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
       }
     </style>
   </head>
   <body>
-    <h1 id="font1"ฟอนต์ของฉัน</h1>
+    <h1 id="font1">บัณฑิตา มังคละแสน เลขที่ 12 ห้อง ม.4/12 เลขประจำตัว 56706</h1>
+    <h1 id="font2">บัณฑิตา มังคละแสน เลขที่ 12 ห้อง ม.4/12 เลขประจำตัว 56706</h1>
   </body>
 </html>
