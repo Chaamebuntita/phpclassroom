f4af72b4331eff2242095c1ef9766bbcb3a2d92a
lsw-56706 <98399961+Chaamebuntita@users.noreply.github.com>
2022-03-02 21:20:36 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -10,13 +10,35 @@
         <th>รูปภาพ</th>
         <th>Twitter/FB</th>
       </tr>
-      <tr>
+       <tr>
         <td>1</td>
-        <td>บัณฑิตา มังคละแสน</td>
-        <td><img src="https://images.app.goo.gl/aWYSHoSVNpKKHCxU8" width="45px"></td>
-        <td><img src="https://picpost.postjung.com/347659.html" width="45px"></td>
-        <td><a href="https://www.facebook.com/Buntita.Mungkalasaen">Facebook</a></td>
+        <td>ใบเฟิร์น พิมพ์ชนก</td>
+        <td><img src="https://images.app.goo.gl/CxAp5zPUD4nmxLtB9"45px"></td>
+        <td><a href="https://https://https://instagram.com/baifernbah?utm_medium=copy_link/">Instagram</a></td>
       </tr>
+       <tr>
+        <td>2</td>
+        <td>มุกดา นรินทร์รักษ์</td>
+        <td><img src="https://images.app.goo.gl/HZjDwjk93Fu7vToS9"45px"></td>
+        <td><a href="https://instagram.com/narinnarinrak?utm_medium=copy_link/">Instagram</a></td>
+      <tr>
+       <tr>
+        <td>3</td>
+        <td>มุก วรนิษฐ์</td>
+        <td><img src="https://images.app.goo.gl/WFGcW3qUg8Saq1km9"></td>
+        <td><a href="https://instagram.com/mookworranit?utm_medium=copy_link/">Instagram</a></td>
+      <tr>
+       <tr>
+        <td>4</td>
+        <td>เก้า สุภัสสรา</td>
+        <td><img src="https://images.app.goo.gl/roQMAFBHFV3YigFy5"></td>
+        <td><a href="https://instagram.com/supassra_sp?utm_medium=copy_link/">Instagram</a></td>
+      <tr>
+       <tr>
+        <td>5</td>
+        <td>ต่อ ธณภพ</td>
+        <td><img src="https://images.app.goo.gl/czZpYCB8kTQNvXFt5"></td>
+        <td><a href="https://instagram.com/thanapob_lee?utm_medium=copy_link/">Instagram</a></td>
     </table>
   </body>
 </html>
